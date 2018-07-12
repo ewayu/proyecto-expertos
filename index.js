c73cb0180dfa7b3f224d38b1a890fc58308e1a7a
ewayu <migue.edu8@gmail.com>
2018-07-12 11:25:54 -0600

    mi primer commit

---
+++ index.js
@@ -0,0 +1,10 @@
+ //Servidor web en nodeJS para publicar archivos estaticos.
+ var express = require("express");
+ var app = express();
+
+ //Exponer una carpeta como publica, unicamente para archivos estaticos: .html, imagenes, .css, .js
+ app.use(express.static("www"));
+
+ //Crear y levantar el servidor web.
+ app.listen(3000);
+ console.log("Servidor iniciado");
